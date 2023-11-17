a1927f476962561618751ef2ac072289dc8fbad6
Mickaël Fontès <81414455+MickaelFontes@users.noreply.github.com>
2023-11-17 14:11:12 +0100

    fix(test): add virtual display

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.chrome.options import Options
+import pytest
+from pyvirtualdisplay import Display
 
 
 def pytest_setup_options():
@@ -7,3 +9,14 @@ def pytest_setup_options():
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1280,720")
     return options
+
+
+@pytest.fixture(autouse=True)
+def web_browser_display():
+    display = Display(visible=False, size=(1920, 1080))
+    display.start()
+
+    # Return browser instance to test case:
+    yield
+
+    display.stop()
