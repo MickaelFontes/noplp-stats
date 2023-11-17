e58a73cf31c8ec58b3e10fa1a34baeb9f5c171f0
Mickaël Fontès <81414455+MickaelFontes@users.noreply.github.com>
2023-11-17 17:04:25 +0100

    fix(test): remove `headless=new` option
    Signed-off-by: Mickaël Fontès <81414455+MickaelFontes@users.noreply.github.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from pyvirtualdisplay import Display
 
 def pytest_setup_options():
     options = Options()
-    options.add_argument("--headless=new")
+    # options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1280,720")
     return options
