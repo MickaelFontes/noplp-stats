f89b85d073bcf33376338c552dd1d8990293944e
Mickaël Fontès <81414455+MickaelFontes@users.noreply.github.com>
2023-11-18 16:10:34 +0100

    fix(test): try remove virtual screen

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,22 @@
 from selenium.webdriver.chrome.options import Options
-import pytest
-from pyvirtualdisplay import Display
+# import pytest
+# from pyvirtualdisplay import Display
 
 
 def pytest_setup_options():
     options = Options()
-    # options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1280,720")
     return options
 
 
-@pytest.fixture(autouse=True)
-def web_browser_display():
-    display = Display(visible=False, size=(1920, 1080))
-    display.start()
+# @pytest.fixture(autouse=True)
+# def web_browser_display():
+#     display = Display(visible=False, size=(1920, 1080))
+#     display.start()
 
-    # Return browser instance to test case:
-    yield
+#     # Return browser instance to test case:
+#     yield
 
-    display.stop()
+#     display.stop()
