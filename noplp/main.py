49cb52e05a2f2f95725f43c9bdcc2c132b78f0e2
Mickaël Fontès <81414455+MickaelFontes@users.noreply.github.com>
2024-01-14 23:37:35 +0100

    style(example): :art: improve code example

--- noplp/main.py
+++ noplp/main.py
@@ -1,30 +1,37 @@
 """Main module."""
 
+import asyncio
+import time
+
+import aiohttp
+
 from noplp.scrapper import Scrapper
 
-import asyncio
-import aiohttp
-import time
 
 async def main():
     """Small test for the Scrapper class."""
     start_time = time.time()
     scrap = Scrapper(singer_required=False)
     tasks = []
-    songs = ("Stéréo", "Tandem", "Dis-moi", "Joe le taxi", "Ton invitation", "Lola", "Il avait les mots")
+    songs = (
+        "Stéréo",
+        "Tandem",
+        "Dis-moi",
+        "Joe le taxi",
+        "Ton invitation",
+        "Lola",
+        "Il avait les mots",
+    )
     session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=5))
+
     for song in songs:
         task = asyncio.create_task(scrap.get_song(page=song, session=session))
         tasks.append(task)
-    # page = "Avenir"
-
-    # print(song_api.title)
-    # print(song_api.singer)
     await asyncio.gather(*tasks)
     await session.close()
 
     time_difference = time.time() - start_time
-    print(f'Scraping time: %.2f seconds.' % time_difference)
+    print(f"Scraping time: {time_difference:.2f} seconds.")
     # for i, cat in enumerate(song_api.categories):
     #     print(cat, ": ", song_api.emissions[i], song_api.dates[i], song_api.points[i])
 
