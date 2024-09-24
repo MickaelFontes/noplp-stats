97418e4dcaf3e662df3a12b735572c54bacf18e7
MickaelFontes <81414455+MickaelFontes@users.noreply.github.com>
2024-09-24 11:21:55 +0200

    fix(tests): :construction: attempt to fix test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 def pytest_setup_options():
     options = Options()
     options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1280,720")
     return options
