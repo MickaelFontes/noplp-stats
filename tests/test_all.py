c5e24dff286e13d65c0f1ebf042b8f3858f1f622
MickaelFontes <81414455+MickaelFontes@users.noreply.github.com>
2024-09-24 13:13:35 +0200

    fix(tests): :construction: sleep before load page

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from dash.testing.composite import DashComposite
 
 from app import app as noplp_app
@@ -7,6 +9,7 @@ def test_all_pages(dash_duo: DashComposite):
     # 4. host the app locally in a thread, all dash server configs could be
     # passed after the first app argument
     dash_duo.start_server(noplp_app, debug=True)
+    sleep(10)
     # 5. use wait_for_* if your target element is the result of a callback,
     # keep in mind even the initial rendering can trigger callbacks
     print(dash_duo.server_url)
