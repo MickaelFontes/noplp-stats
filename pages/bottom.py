87d2dd8b7850dde8660d99073aaa4dfa05299884
MickaelFontes <81414455+MickaelFontes@users.noreply.github.com>
2025-11-21 01:14:46 +0000

    chore(database): update database

--- pages/bottom.py
+++ pages/bottom.py
@@ -15,7 +15,7 @@ bottom = html.Div(
                 " ![Github](assets/images/favicon.svg#thumbnail) ",
                 "[noplp-stats](https://github.com/MickaelFontes/noplp-stats) sous ",
                 "[MIT License](https://github.com/MickaelFontes/noplp-stats/blob/main/LICENSE) - ",  # noqa: E501
-                "Dernière mise à jour: le 09/11/2025",
+                "Dernière mise à jour: le 21/11/2025",
             ]
         ),
     ],
