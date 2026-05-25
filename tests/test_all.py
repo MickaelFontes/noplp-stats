8adbb08fc53f77b2d94b8f3e5c6bc1c507997fa7
MickaelFontes <81414455+MickaelFontes@users.noreply.github.com>
2026-05-25 20:10:54 +0000

    test: :construction: update tests to remove `dash[testing]`

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,24 +1,34 @@
-from dash.testing.composite import DashComposite
+from urllib.parse import urljoin
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
 from app import app as noplp_app
 
 
-def test_all_pages(dash_duo: DashComposite):
-    # 4. host the app locally in a thread, all dash server configs could be
-    # passed after the first app argument
-    dash_duo.start_server(noplp_app)
-    # 5. use wait_for_* if your target element is the result of a callback,
-    # keep in mind even the initial rendering can trigger callbacks
-    print(dash_duo.server_url)
-    dash_duo.wait_for_page()
-    assert dash_duo.get_logs() == [], "browser console should contain no error"
-    dash_duo.wait_for_page(dash_duo.server_url + "/global")
-    assert dash_duo.get_logs() == [], "browser console should contain no error"
-    dash_duo.wait_for_page(dash_duo.server_url + "/category")
-    assert dash_duo.get_logs() == [], "browser console should contain no error"
-    dash_duo.wait_for_page(dash_duo.server_url + "/song")
-    assert dash_duo.get_logs() == [], "browser console should contain no error"
-    dash_duo.wait_for_page(dash_duo.server_url + "/singer")
-    assert dash_duo.get_logs() == [], "browser console should contain no error"
-    dash_duo.wait_for_page(dash_duo.server_url + "/training")
-    assert dash_duo.get_logs() == [], "browser console should contain no error"
+def test_all_pages(browser, live_server):
+    routes = [
+        ("/", "NOPLP stats - Statistiques N'oubliez pas les paroles"),
+        ("/global", "Global - NOPLP stats - Statistiques N'oubliez pas les paroles"),
+        (
+            "/category",
+            "Par catégorie - NOPLP stats - Statistiques N'oubliez pas les paroles",
+        ),
+        ("/song", "Par chanson - NOPLP stats - Statistiques N'oubliez pas les paroles"),
+        (
+            "/singer",
+            "Par interprète - NOPLP stats - Statistiques N'oubliez pas les paroles",
+        ),
+        (
+            "/training",
+            "Entraînement - NOPLP stats - Statistiques N'oubliez pas les paroles",
+        ),
+    ]
+
+    assert noplp_app.title == "NOPLP stats - Statistiques N'oubliez pas les paroles"
+
+    for path, expected_title in routes:
+        browser.get(urljoin(live_server, path))
+        WebDriverWait(browser, 10).until(ec.title_is(expected_title))
+        assert browser.find_element(By.ID, "navbar-toggler")
